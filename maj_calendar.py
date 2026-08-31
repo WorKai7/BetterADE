149b11bff754d01b994bf7fe716a4a7092d115b4
WorKai7 <jvcraft59@gmail.com>
2026-08-31 16:23:26 +0200

    feat(update): add timeout

--- maj_calendar.py
+++ maj_calendar.py
@@ -1,6 +1,11 @@
 import requests
 
-response = requests.get("https://edt.univ-littoral.fr/jsp/custom/modules/plannings/9n9Rr7WP.shu")
+response = requests.get(
+    "https://edt.univ-littoral.fr/jsp/custom/modules/plannings/9n9Rr7WP.shu",
+    timeout=30
+)
+
+response.raise_for_status()
 
 
 with open("planning.shu", "wb") as f:
